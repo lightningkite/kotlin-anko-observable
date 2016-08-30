e38dd1845b0b393a0ff906993dd6c647ce8d36c3
Joseph Ivie <josephivie@gmail.com>
2016-08-30 10:37:28 -0600

    Error with retaining spinner selection in Advanced Vehicle Search fixed

--- src/main/java/com/lightningkite/kotlin/anko/observable/adapter/StandardListAdapter.kt
+++ src/main/java/com/lightningkite/kotlin/anko/observable/adapter/StandardListAdapter.kt
@@ -10,10 +10,13 @@ import android.widget.Spinner
 import com.lightningkite.kotlin.anko.lifecycle
 import com.lightningkite.kotlin.lifecycle.listen
 import com.lightningkite.kotlin.observable.list.ObservableList
+import com.lightningkite.kotlin.observable.property.MutableObservableProperty
 import com.lightningkite.kotlin.observable.property.ObservableProperty
 import com.lightningkite.kotlin.observable.property.StandardObservableProperty
+import com.lightningkite.kotlin.observable.property.bind
 import org.jetbrains.anko.AnkoContextImpl
 import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.onItemSelectedListener
 import org.jetbrains.anko.wrapContent
 
 /**
@@ -56,8 +59,8 @@ open class StandardListAdapter<T>(
             val obs = convertView.tag as ItemObservable<T>
             obs.index = position
             obs.value = (list[position])
+            return convertView
         }
-        return convertView
     }
 
     class SLVAContext<T>(adapter: StandardListAdapter<T>, context: Context) : AnkoContextImpl<StandardListAdapter<T>>(context, adapter, false) {
@@ -125,6 +128,53 @@ inline fun <T> Spinner.standardAdapter(
     adapter = newAdapter
     lifecycle.listen(list.onUpdate) {
         newAdapter.notifyDataSetChanged()
+    }
+    return newAdapter
+}
+
+inline fun <T> Spinner.standardAdapter(
+        list: ObservableList<T>,
+        selected: MutableObservableProperty<T>,
+        noinline makeView: StandardListAdapter.SLVAContext<T>.(StandardListAdapter.ItemObservable<T>) -> Unit
+): StandardListAdapter<T> {
+    val newAdapter: StandardListAdapter<T> = StandardListAdapter(context, list, makeView)
+    adapter = newAdapter
+
+    var indexAlreadySet = false
+
+    lifecycle.listen(list.onUpdate) {
+        newAdapter.notifyDataSetChanged()
+        val index = list.indexOf(selected.value)
+        println("update to $index - ${selected.value}")
+        if (index == -1) {
+            println("could not find ${selected.value}")
+            setSelection(0)
+            return@listen
+        }
+        setSelection(index)
+    }
+
+    lifecycle.bind(selected) { it ->
+        val index = list.indexOf(it)
+        println("selected to $index - $it")
+        if (index == -1) {
+            println("could not find $it")
+            setSelection(0)
+            return@bind
+        }
+        if (!indexAlreadySet) {
+            setSelection(index)
+        } else {
+            indexAlreadySet = false
+        }
+    }
+
+    this.onItemSelectedListener {
+        onItemSelected { adapterView, view, index, id ->
+            println("set to $index - ${list[index]}")
+            indexAlreadySet = true
+            selected.value = (list[index])
+        }
     }
     return newAdapter
 }
