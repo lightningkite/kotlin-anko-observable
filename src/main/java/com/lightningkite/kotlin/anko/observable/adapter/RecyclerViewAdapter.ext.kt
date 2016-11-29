2c09e43da11e877694c389244517a15fa7252987
Joseph Ivie <josephivie@gmail.com>
2016-11-28 22:45:04 -0700

    Grazing stuff

--- src/main/java/com/lightningkite/kotlin/anko/observable/adapter/RecyclerViewAdapter.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/observable/adapter/RecyclerViewAdapter.ext.kt
@@ -37,6 +37,38 @@ fun <ITEM, VH : RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.attachAnimatio
     list.addListenerSet(newSet.second)
 }
 
+fun <ITEM, VH : RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.attachAnimationsMin1(list: ObservableList<ITEM>) {
+    detatchAnimations<ITEM, VH>()
+    val newSet = list to ObservableListListenerSet(
+            onAddListener = { item: ITEM, position: Int ->
+                if (list.size == 1) {
+                    notifyItemChanged(position)
+                } else {
+                    notifyItemInserted(position)
+                }
+            },
+            onRemoveListener = { item: ITEM, position: Int ->
+                //ensures there is always at least one element shown
+                if (list.isEmpty()) {
+                    notifyItemChanged(position)
+                } else {
+                    notifyItemRemoved(position)
+                }
+            },
+            onChangeListener = { oldItem: ITEM, item: ITEM, position: Int ->
+                notifyItemChanged(position)
+            },
+            onMoveListener = { item: ITEM, oldPosition: Int, position: Int ->
+                notifyItemMoved(oldPosition, position)
+            },
+            onReplaceListener = { list: ObservableList<ITEM> ->
+                notifyDataSetChanged()
+            }
+    )
+    previousListenerSet[this] = newSet
+    list.addListenerSet(newSet.second)
+}
+
 @Suppress("UNCHECKED_CASt")
 fun <ITEM, VH : RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.detatchAnimations() {
     val prev = previousListenerSet[this]
@@ -49,6 +81,18 @@ fun <ITEM, VH : RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.attachAnimatio
     lifecycle.connect(object : LifecycleListener {
         override fun onStart() {
             attachAnimations<ITEM, VH>(list)
+        }
+
+        override fun onStop() {
+            detatchAnimations<ITEM, VH>()
+        }
+    })
+}
+
+fun <ITEM, VH : RecyclerView.ViewHolder> RecyclerView.Adapter<VH>.attachAnimationsMin1(lifecycle: LifecycleConnectable, list: ObservableList<ITEM>) {
+    lifecycle.connect(object : LifecycleListener {
+        override fun onStart() {
+            attachAnimationsMin1<ITEM, VH>(list)
         }
 
         override fun onStop() {
