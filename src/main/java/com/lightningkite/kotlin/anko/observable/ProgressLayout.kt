8b6a4be24ce631b7526c803e365a663a44e18bd7
Joseph Ivie <josephivie@gmail.com>
2017-05-25 13:23:00 -0600

    GroupVC complete

--- src/main/java/com/lightningkite/kotlin/anko/observable/ProgressLayout.kt
+++ src/main/java/com/lightningkite/kotlin/anko/observable/ProgressLayout.kt
@@ -2,7 +2,7 @@ package com.lightningkite.kotlin.anko.observable
 
 import android.view.Gravity
 import android.view.View
-import android.view.ViewGroup
+import android.view.ViewManager
 import android.widget.FrameLayout
 import com.lightningkite.kotlin.anko.animation.TransitionView
 import com.lightningkite.kotlin.anko.animation.transitionView
@@ -11,22 +11,22 @@ import com.lightningkite.kotlin.observable.property.MutableObservableProperty
 import com.lightningkite.kotlin.observable.property.ObservableProperty
 import com.lightningkite.kotlin.observable.property.StandardObservableProperty
 import com.lightningkite.kotlin.observable.property.bind
-import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.progressBar
 import org.jetbrains.anko.wrapContent
 
 /**
  * Created by jivie on 7/15/16.
  */
-fun ViewGroup.progressLayout(
+fun ViewManager.progressLayout(
         runningObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false),
+        progressSize: Int = wrapContent,
         otherViewMaker: TransitionView.(running: MutableObservableProperty<Boolean>) -> View
 ): TransitionView {
     return transitionView() {
         otherViewMaker(runningObs).tag("content").apply {
             (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
         }
-        progressBar().lparams(wrapContent, wrapContent).tag("loading").apply {
+        progressBar().lparams(progressSize, progressSize).tag("loading").apply {
             (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
         }
 
@@ -37,31 +37,12 @@ fun ViewGroup.progressLayout(
     }
 }
 
-fun ViewGroup.progressLayoutReadOnly(
+fun ViewManager.progressLayoutReadOnly(
         runningObs: ObservableProperty<Boolean>,
         otherViewMaker: TransitionView.() -> View
 ): TransitionView {
     return transitionView() {
         otherViewMaker().tag("content").apply {
-            (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
-        }
-        progressBar().lparams(wrapContent, wrapContent).tag("loading").apply {
-            (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
-        }
-
-        lifecycle.bind(runningObs) { loading ->
-            if (loading) animate("loading")
-            else animate("content")
-        }
-    }
-}
-
-fun AnkoContext<*>.progressLayout(
-        runningObs: MutableObservableProperty<Boolean> = StandardObservableProperty(false),
-        otherViewMaker: TransitionView.(running: MutableObservableProperty<Boolean>) -> View
-): TransitionView {
-    return transitionView() {
-        otherViewMaker(runningObs).tag("content").apply {
             (layoutParams as FrameLayout.LayoutParams).gravity = Gravity.CENTER
         }
         progressBar().lparams(wrapContent, wrapContent).tag("loading").apply {
